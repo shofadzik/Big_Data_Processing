e6fff5032e84770475d5f17a81ab24d7caf809d8
Muhammad Shofa Dzikron <107288295+shofadzik@users.noreply.github.com>
2022-09-12 10:57:56 +0700

    Update get_covid.py

--- get_covid.py
+++ get_covid.py
@@ -7,4 +7,3 @@ df = spark.read.option("header",True).csv("source/covid.csv")
 df.show()
 df.write.json('result/shofa')
 
-#intinya program ini akan copy file csv dari source/covid.csv ke result/<nama> dengan format json
